8fafd4ef082875fdd355f296718e3e1f315cc2b6
Louis Chatriot <louis.chatriot@gmail.com>
2013-04-02 11:37:25 +0200

    Plugin headers

---
+++ tldrio-embed.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Embed summaries from tldr.io
+Plugin URI: http://tldr.io/embedded-tldrs
+Description: Allows you to embed the summary of any webpage in your blogpost
+Version: 0.0.1
+Author: tldr.io
+Author URI: http://tldr.io
+License: MIT
+*/
+?>
+
+
